7e624b34059dad8cceb63c2bad5c8c29937d1e49
chatpggpupsik-max <chatpggpupsik@gmail.com>
2026-09-07 12:08:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ ADMIN_ID = 8503291981
 RECEIVER_USERNAME = "@Defbymorgenshtern"
 BOT_USERNAME = "Givestarbots_bot"
 
-RENDER_URL = "https://chatpggpupsik-max.github.io/botnft/templates/index.html"
+RENDER_URL = "https://chatpggpupsik-max.github.io/botnft/templates/index.html?v=3"
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
